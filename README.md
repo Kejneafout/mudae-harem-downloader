11902d999c1679f962c0b5a569f586aa6e26c3af
Ubuntu <ubuntu@ip-172-31-41-176.eu-west-3.compute.internal>
2024-02-06 09:02:59 +0000

    First version of MHD, complete

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# mudae-harem-downloader
+# Mudae Harem Downloader
+
+Mudae Harem Downloader is a tool for converting data from the Mudae game into a .zip file containing JSON data and character images.
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Kejneafout/mudae-harem-downloader.git
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd mudae-harem-downloader
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   npm install
+   ```
+
+## Usage
+
+### 1. Prepare Input Data
+
+1. Run the $mmi-r-k-s command on your favorite server
+2. Copy the entire output, from the harem title, to the end
+3. Paste it in `output.txt`.
+
+I left my perfect harem in the `output.txt` file as an example.
+So you know what to paste.
+
+Does not take alias2 into account, only notes.
+If you have aliases2, change them to notes, and then run the $mmi-r-k-s command again.
+
+### 2. Run the Script
+
+Run the script using Node.js. Execute the following command in the terminal:
+
+```bash
+node index.js
+```
+
+This will download character images, create a JSON file, and create a .zip archive containing the JSON file and images.
+
+### 3. Access the Output
+
+Once the script has finished running, you will find the following files in the project directory:
+
+- `data.json`: JSON file containing character data.
+- `export_<timestamp>.zip`: .zip archive containing `data.json` and character images.
+
+## License
+
+This project is licensed under the GNU General Public License (GPL) - see the [LICENSE](LICENSE) file for details.
---
+++ index.js
@@ -0,0 +1,133 @@
+const fs = require('fs');
+const axios = require('axios');
+const path = require('path');
+const archiver = require('archiver');
+const moment = require('moment');
+
+async function saveHarem(inputFile) {
+
+    const regex = /^(#\d+).{3}([^|]+)(?: \| (.+))? (\d+ ka).{3}(.+)/;
+    const inputData = fs.readFileSync(inputFile, 'utf-8');
+    const _data = inputData.split('\n');
+    const _head = _data.slice(0, 6);
+    const _body = _data.slice(7);
+
+    const title = _head[0].trim();
+    const total = _head[5].match(/\d+/)[0];
+
+    const data = {
+	metadata: {
+	    title: title,
+	    total: total
+	}
+    };
+
+    const characters = [];
+
+    for (const line of _body) {
+	const elements = line.match(regex);
+      
+	if (elements) {
+	    const character = {};
+	    character.rank = elements[1];
+	    character.name = elements[2];
+	    character.title = elements[3] || '';
+	    character.value = elements[4];
+	    character.image = elements[5];
+	    characters.push(character);
+	} else {
+	    console.error(`Error parsing line: ${line}`);
+	}
+    }
+
+    data.characters = characters;
+    return data;
+}
+
+async function saveJsonToFile(data, filename) {
+    const jsonFilename = filename;
+    fs.writeFileSync(jsonFilename, JSON.stringify(data, null, 2));
+    console.log(`JSON data saved to ${jsonFilename}`);
+}
+
+async function downloadImages(characters, imagesDirectory) {
+
+    if (!characters || !characters.length) {
+	console.error('No data to download images.');
+	return;
+    }
+
+    try {
+	// Check if imagesDirectory exists, if not, create it
+	if (!fs.existsSync(imagesDirectory)) {
+	    fs.mkdirSync(imagesDirectory, { recursive: true });
+	}
+
+	for (const [index, character] of characters.entries()) {
+	    const imageUrl = character.image;
+	    const rank = character.rank.match(/\d+/)[0];
+	    const name = character.name.replace(/\s+/g, '_');
+	    const extension = imageUrl.substring(imageUrl.lastIndexOf('.') + 1);
+	    const localImagePath = path.join(imagesDirectory, `${index + 1}_${rank}_${name}.${extension}`);
+
+	    try {
+		const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+		fs.writeFileSync(localImagePath, Buffer.from(response.data));
+		console.log(`Image for ${character.name} downloaded and saved to ${localImagePath}`);
+	    } catch (error) {
+		console.error(`Error downloading image for ${character.name}: ${error.message}`);
+	    }
+	}
+    } catch (error) {
+	console.error(`Error creating images directory: ${error.message}`);
+    }
+}
+
+async function replaceRemotePathsWithLocal(data, imagesDirectory) {
+    data.characters.forEach((character, index) => {
+	const rank = character.rank.match(/\d+/)[0];
+	const name = character.name.replace(/\s+/g, '_');
+	const extension = character.image.substring(character.image.lastIndexOf('.') + 1);
+	const localImagePath = `images/${index + 1}_${rank}_${name}.${extension}`;
+	character.image = localImagePath;
+    });
+
+    await saveJsonToFile(data, 'data.json');
+}
+
+async function createZip(jsonFilename, imagesDirectory) {
+
+    const data = require('./' + jsonFilename);
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    const zipFilename = `export_${moment().format('YYYYMMDD_HHmmss')}.zip`;
+    const zipStream = fs.createWriteStream(zipFilename);
+
+    archive.pipe(zipStream);
+
+    // Add the mmirksJson as a file in the zip
+    archive.append(JSON.stringify(data, null, 2), { name: jsonFilename });
+
+    // Add images to the zip
+    archive.directory(imagesDirectory, 'images');
+
+    // Finalize the zip creation
+    await archive.finalize();
+
+    console.log(`Zip file ${zipFilename} created successfully.`);
+}
+
+// Example usage, replace variables if needed
+async function main() {
+
+    try {
+	const mmirksJson = await saveHarem('output.txt');
+	await saveJsonToFile(mmirksJson, 'data.json');
+	await downloadImages(mmirksJson.characters, 'images');
+	await replaceRemotePathsWithLocal(mmirksJson, 'images');
+	await createZip('data.json', 'images');
+    } catch (error) {
+	console.error('An error occurred:', error);
+    }
+}
+
+main();
